9f58701831d1ba9ef10851517a6fe0d125d9272a
Egemen Celik <egemenc2101@gmail.com>
2023-09-26 08:32:26 +0300

    navigation and sign in form css changes

--- src/layout/navigation/Navigation.tsx
+++ src/layout/navigation/Navigation.tsx
@@ -1,6 +1,3 @@
-// interface NavigationProps {}
-import { FaCrown } from 'react-icons/fa6'
-import { BsThreeDotsVertical } from 'react-icons/bs'
 import triCountLogo from '../../assets/tricount_blanc.png'
 import './navigation.styles.scss'
 import { useAppSelector } from '../../hooks'
@@ -14,11 +11,7 @@ export default function Navigation() {
   return (
     <nav>
       <img src={triCountLogo} alt="triCountLogo" />
-      <div className="icon-container">
-        <FaCrown size={25} />
-        <BsThreeDotsVertical size={25} />
-        {userEmail ? <NavAuthItem userEmail={userEmail} /> : null}
-      </div>
+      {userEmail ? <NavAuthItem userEmail={userEmail} /> : null}
     </nav>
   )
 }
